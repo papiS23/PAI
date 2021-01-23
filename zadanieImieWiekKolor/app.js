6f8d7ea1fcc4c02733bcd98f865c5a4884fe0ac8
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-01-23 01:06:39 +0100

    poprawka

--- zadanieImieWiekKolor/app.js
+++ zadanieImieWiekKolor/app.js
@@ -27,52 +27,56 @@ button.addEventListener('click', e=>{
     }
     
     //Name check
-    if(!(isNaN(firstLetter[0]))){
-        inputs[0].value = '';
-        alert('Pierwszy znak imienia nie może być cyfrą!');
-    }else if(firstLetter.length<3){
-        inputs[0].value = '';
-        alert('Imię musi mieć więcej niż 3 znaki!');
-    }
-    else{
-        let joinName = firstLetter.join('');
-        const finalName = joinName[0].toUpperCase() + joinName.substr(1);
-
-        //Color
-        const color = inputs[2].value.toLowerCase();
-        const hex =['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','#'];
-
-
-        for(let i=0; i<color.length; i++){
-            if(!hex.includes(color[i])){
-                console.log(color[i]);
-                inputs[2]='';
-                alert('Kolor podaj w systemie szesnastkowym!');
-                break;
-            }else if(i===color.length-1){
-                if(!(color[0]==="#")){
-                    alert('Pamiętaj o # na początku.');
-                }else if(color.length<7 || color.length>7){
-                    inputs[2]='';
-                    alert('Ilość znaków twojego koloru jest niepoprawna!');
-                }else{
-                    switch(true){
-                        case age>18:
-                            result.innerHTML = `Cześć ${finalName}, jesteś ${sex} od ${age-18} ${year}.`
-                            result.style.border = `solid 1px ${color}`;
-                            break;
-                        case age==18:
-                            result.innerHTML = `Cześć ${finalName}, jesteś ${sex}.`
-                            result.style.border = `solid 1px ${color}`;
-                            break;
-                        case age<18:
-                            result.innerHTML = `Cześć ${finalName}, do pełnoletniości brakuje ci ${18-age} ${year}.`;
-                            result.style.border = `solid 1px ${color}`;
-                            break;
-                    }
-
+    for(let k=0; k<firstLetter.length;k++){
+        if(!(isNaN(firstLetter[k]))){
+            result.innerHTML = 'Imiona nie mogą zawierać cyfr!';
+            result.style.border = `solid 1px #ff0000`;
+        }else if(k===firstLetter.length-1){
+            if(firstLetter.length<3){
+                result.innerHTML = 'Imię musi mieć więcej niż 3 znaki!';
+                result.style.border = `solid 1px #ff0000`;
                 }
-            }
+                else{
+                    let joinName = firstLetter.join('');
+                    const finalName = joinName[0].toUpperCase() + joinName.substr(1);
+            
+                    //Color
+                    const color = inputs[2].value.toLowerCase();
+                    const hex =['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','#'];
+            
+            
+                    for(let i=0; i<color.length; i++){
+                        if(!hex.includes(color[i])){
+                            console.log(color[i]);
+                            result.innerHTML='Kolor podaj w systemie szesnastkowym!';
+                            result.style.border = `solid 1px #ff0000`;
+                            break;
+                        }else if(i===color.length-1){
+                            if(!(color[0]==="#")){
+                                result.innerHTML='Pamiętaj o # na początku.';
+                            }else if(color.length<7 || color.length>7){
+                                result.innerHTML='Ilość znaków twojego koloru jest niepoprawna!';
+                                result.style.border = `solid 1px #ff0000`;
+                            }else{
+                                switch(true){
+                                    case age>18:
+                                        result.innerHTML = `Cześć ${finalName}, jesteś ${sex} od ${age-18} ${year}.`
+                                        result.style.border = `solid 1px ${color}`;
+                                        break;
+                                    case age==18:
+                                        result.innerHTML = `Cześć ${finalName}, jesteś ${sex}.`
+                                        result.style.border = `solid 1px ${color}`;
+                                        break;
+                                    case age<18:
+                                        result.innerHTML = `Cześć ${finalName}, do pełnoletniości brakuje ci ${18-age} ${year}.`;
+                                        result.style.border = `solid 1px ${color}`;
+                                        break;
+                                }
+            
+                            }
+                        }
+                    }
+                }
         }
     }    
 })
