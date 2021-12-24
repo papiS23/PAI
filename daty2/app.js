d006ca43cd973739ebe1b5271c166bfc2a8169d2
Bartłomiej Papis <72127774+papiS23@users.noreply.github.com>
2021-12-24 11:49:45 +0100

    just for fun

--- daty2/app.js
+++ daty2/app.js
@@ -1,12 +1,17 @@
 const hourLine = document.querySelector(".hour-line");
 const minuteLine = document.querySelector(".minute-line");
 const secondLine = document.querySelector(".second-line");
+const numClock = document.querySelector(".number-clock");
 
 setInterval(function () {
   let now = new Date();
   let seconds = now.getSeconds();
   let minute = seconds / 60 + now.getMinutes();
   let hour = minute / 60 + now.getHours();
+
+  numClock.innerText = `${Math.floor(hour)}:${Math.floor(minute)}:${
+    Math.floor(seconds) < 10 ? "0" + Math.floor(seconds) : Math.floor(seconds)
+  }`;
 
   let hourDeg = 30 * (hour > 12 ? hour - 12 : hour);
   let minuteDeg = 6 * minute;
